1c353af1d0f49c1fc8f7db976a0796426630a827
kumarayush0104 <157412992+Annapurnaindia@users.noreply.github.com>
2025-04-06 03:59:12 +0530

    ok

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,52 +1,52 @@
 import React from 'react'
-import { createBrowserRouter, createRoutesFromElements, RouterProvider, Route, Routes } from 'react-router-dom'
-import Login from './pages/Login.jsx';
-import Home from './pages/Home.jsx';
-import Signup from './pages/SignUp.jsx';
-import Dashboard from './pages/Dashboard.jsx';
-import GenerateQR from './pages/GenerateQR.jsx';
-import ProtectedRoute from './components/ProtectedRoute.jsx';
-import Print from './pages/Print.jsx';
+import { createBrowserRouter, createRoutesFromElements, RouterProvider, Route } from 'react-router-dom'
+import Login from './pages/Login.jsx'
+import Home from './pages/Home.jsx'
+import Signup from './pages/SignUp.jsx'
+import Dashboard from './pages/Dashboard.jsx'
+import GenerateQR from './pages/GenerateQR.jsx'
+import ProtectedRoute from './components/ProtectedRoute.jsx'
+import Print from './pages/Print.jsx'
 
 function App() {
-  
-
-    const router = createBrowserRouter(
-      createRoutesFromElements(
-        <Route>
-          <Route path='/' element={<Home/>} />
-          <Route path='/login' element={<Login/>} />
-          <Route path='/signup' element={<Signup/>} />
-          <Route 
-            path="/dashboard" 
-            element={
-              <ProtectedRoute>
-                <Dashboard />
-              </ProtectedRoute>
-            } 
-          />
-          <Route 
-            path="/generate-qr" 
-            element={
-              <ProtectedRoute>
-                <GenerateQR />
-              </ProtectedRoute>
-            } 
-          />
-          <Route
+  const router = createBrowserRouter(
+    createRoutesFromElements(
+      <Route path="/">
+        <Route index element={<Home/>} />
+        <Route path="login" element={<Login/>} />
+        <Route path="signup" element={<Signup/>} />
+        <Route 
+          path="/dashboard" 
+          element={
+            <ProtectedRoute>
+              <Dashboard />
+            </ProtectedRoute>
+          } 
+        />
+        <Route 
+          path="/generate-qr" 
+          element={
+            <ProtectedRoute>
+              <GenerateQR />
+            </ProtectedRoute>
+          } 
+        />
+        <Route
           path="/print/:fileId"
           element={
+            <ProtectedRoute>
               <Print/>
+            </ProtectedRoute>
           }
-          />
-          <Route path="*" element={<Home/>} />
-        </Route>
-      ) 
-    );
+        />
+        <Route path="*" element={<Home/>} />
+      </Route>
+    )
+  )
 
   return (
-      <RouterProvider router={router} />
-    );
+    <RouterProvider router={router} />
+  )
 }
 
 export default App
